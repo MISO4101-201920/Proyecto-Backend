4578851c85bf0a4967f6348b5172abe7b2f8a7fa
carlos daza <carlos.daza@alkanza.us>
2019-11-08 14:15:17 -0500

    fix tests

--- interactive_content/tests.py
+++ interactive_content/tests.py
@@ -48,9 +48,8 @@ class InteractiveContentTestCase(TestCase):
         self.client = APIClient()
         self.user = Profesor.objects.create_superuser('admin', 'admin@admin.com', 'admin123')
         self.token = Token.objects.create(user=self.user)
-        self.url = '/content/interactivecontent/1'
-        self.headers = {'Authorization': 'Token {}'.format(self.token.key),
-                        'Content-Type': 'application/json'}
+        self.url = '/content/interactivecontent/{}'
+        self.headers = {'Content-Type': 'application/json'}
 
     def test_add_interactive_content(self):
         url = '/content/cont_interactivo'
@@ -77,58 +76,61 @@ class InteractiveContentTestCase(TestCase):
         self.assertEqual(response.status_code, 401)
 
     def test_get_interactive_content_200_status(self):
-
-        response = self.client.get(self.url, format='json', headers=self.headers)
+        content = Contenido.objects.create(url="youtube.com", nombre="Mi primer contenido", profesor=self.user)
+        interactive_content = ContenidoInteractivo.objects.create(contenido=content, fecha_creacion=datetime.now(),
+                                                                  tiene_retroalimentacion=True,
+                                                                  nombre="Mi primer contenido")
+        response = self.client.get(self.url.format(interactive_content.id), format='json', headers=self.headers,
+                                   HTTP_AUTHORIZATION='Token ' + self.token.key)
         self.assertEqual(response.status_code, 200)
 
     def test_get_interactive_content_info(self):
-        user = Profesor.objects.create(username="profesor.prueba", password="kda-qwerty-321", first_name="Profesor", last_name="Pruebas", email="a.acostag@uniandes.edu.co")
-
-        content = Contenido.objects.create(url="youtube.com", nombre="Mi primer contenido", profesor=user)
-        interactive_content = ContenidoInteractivo.objects.create(contenido=content, fecha_creacion=datetime.now(), tiene_retroalimentacion=True)
-        response = self.client.get(self.url, format='json', headers=self.headers)
+        content = Contenido.objects.create(url="youtube.com", nombre="Mi primer contenido", profesor=self.user)
+        interactive_content = ContenidoInteractivo.objects.create(contenido=content, fecha_creacion=datetime.now(),
+                                                                  tiene_retroalimentacion=True,
+                                                                  nombre="Mi primer contenido")
+        response = self.client.get(self.url.format(interactive_content.id), format='json', headers=self.headers,
+                                   HTTP_AUTHORIZATION='Token ' + self.token.key)
         data = json.loads(response.content)
 
-        self.assertEqual(data[0]['fields']['nombre'], content.nombre)
-        self.assertEqual(data[0]['fields']['tieneRetroalimentacion'], interactive_content.tiene_retroalimentacion)
-        self.assertEqual(data[0]['fields']['contenido']['nombre'], content.nombre)
-        self.assertEqual(data[0]['fields']['contenido']['url'], content.url)
+        self.assertEqual(data['nombre'], interactive_content.nombre)
+        self.assertEqual(data['tiene_retroalimentacion'], interactive_content.tiene_retroalimentacion)
+        self.assertEqual(data['contenido']['nombre'], content.nombre)
+        self.assertEqual(data['contenido']['url'], content.url)
 
     def test_get_courses_from_interactive_content(self):
-        user = Profesor.objects.create(username="profesor.prueba", password="kda-qwerty-321", first_name="Profesor", last_name="Pruebas", email="a.acostag@uniandes.edu.co")
-
-        content = Contenido.objects.create(url="youtube.com", nombre="Mi primer contenido", profesor=user)
+        content = Contenido.objects.create(url="youtube.com", nombre="Mi primer contenido", profesor=self.user)
         interactive_content = ContenidoInteractivo.objects.create(contenido=content, fecha_creacion=datetime.now(), tiene_retroalimentacion=True)
 
-        curso1 = Curso.objects.create(fecha_creacion=datetime.now(), nombre="Mi primer curso", profesor=user, descripcion="Breve descripcion 1")
-        curso2 = Curso.objects.create(fecha_creacion=datetime.now(), nombre="Mi segundo curso", profesor=user, descripcion="Breve descripcion 2")
+        curso1 = Curso.objects.create(fecha_creacion=datetime.now(), nombre="Mi primer curso", profesor=self.user, descripcion="Breve descripcion 1")
+        curso2 = Curso.objects.create(fecha_creacion=datetime.now(), nombre="Mi segundo curso", profesor=self.user, descripcion="Breve descripcion 2")
 
         interactive_content = ContenidoInteractivo.objects.create(contenido=content, fecha_creacion=datetime.now(),
                                                                   tiene_retroalimentacion=True)
         interactive_content.curso.add(curso1)
         interactive_content.curso.add(curso2)
 
-        response = self.client.get(self.url, format='json', headers=self.headers)
+        response = self.client.get(self.url.format(interactive_content.id), format='json', headers=self.headers,
+                                   HTTP_AUTHORIZATION='Token ' + self.token.key)
         data = json.loads(response.content)
 
-        self.assertEqual(data[0]['fields']['cursos'][0]['nombre'], interactive_content.curso.first().nombre)
-        self.assertEqual(data[0]['fields']['cursos'][1]['nombre'], interactive_content.curso.last().nombre)
-        self.assertEqual(data[0]['fields']['cursos'][0]['descripcion'], interactive_content.curso.first().descripcion)
-        self.assertEqual(data[0]['fields']['cursos'][1]['descripcion'], interactive_content.curso.last().descripcion)
+        self.assertEqual(data['cursos'][0]['nombre'], interactive_content.curso.first().nombre)
+        self.assertEqual(data['cursos'][1]['nombre'], interactive_content.curso.last().nombre)
+        self.assertEqual(data['cursos'][0]['descripcion'], interactive_content.curso.first().descripcion)
+        self.assertEqual(data['cursos'][1]['descripcion'], interactive_content.curso.last().descripcion)
 
     def test_get_marcas_from_interactive_content(self):
-        user = Profesor.objects.create(username="profesor.prueba", password="kda-qwerty-321", first_name="Profesor", last_name="Pruebas", email="a.acostag@uniandes.edu.co")
-
-        content = Contenido.objects.create(url="youtube.com", nombre="Mi primer contenido", profesor=user)
+        content = Contenido.objects.create(url="youtube.com", nombre="Mi primer contenido", profesor=self.user)
         interactive_content = ContenidoInteractivo.objects.create(contenido=content, fecha_creacion=datetime.now(), tiene_retroalimentacion=True)
 
         marca1 = Marca.objects.create(nombre="Marca 1", punto=100, contenido=interactive_content)
         marca2 = Marca.objects.create(nombre="Marca 2", punto=200, contenido=interactive_content)
 
-        response = self.client.get(self.url, format='json', headers=self.headers)
+        response = self.client.get(self.url.format(interactive_content.id), format='json', headers=self.headers,
+                                   HTTP_AUTHORIZATION='Token ' + self.token.key)
         data = json.loads(response.content)
 
-        self.assertEqual(data[0]['fields']['marcas'][0]['id'], 1)
-        self.assertEqual(data[0]['fields']['marcas'][0]['punto'], marca1.punto)
-        self.assertEqual(data[0]['fields']['cursos'][1]['id'], 2)
-        self.assertEqual(data[0]['fields']['cursos'][1]['punto'], marca2.punto)
+        self.assertEqual(data['marcas'][0]['id'], 1)
+        self.assertEqual(data['marcas'][0]['punto'], marca1.punto)
+        self.assertEqual(data['marcas'][1]['id'], 2)
+        self.assertEqual(data['marcas'][1]['punto'], marca2.punto)
