59796cf6244d3fa42d991d5829072ed6a161cb23
Armando Acosta Guerrero <darman.89@gmail.com>
2019-11-08 12:27:23 -0500

    Heroku workaround (codeship)

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # MISO4101-201920 - Backend - Proyecto base
+[![Codeship Status for darman89/Proyecto-Backend](https://app.codeship.com/projects/f5639760-e3b4-0137-1c8c-42edbe01be32/status?branch=Sprint2-release)](https://app.codeship.com/projects/373124)
--- main/settings.py
+++ main/settings.py
@@ -14,7 +14,6 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import dj_database_url
-import django_heroku
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -130,8 +129,15 @@ STATICFILES_DIRS = (
 # https://warehouse.python.org/project/whitenoise/
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-django_heroku.settings(locals())
-# sslmode issue workaround
+# heroku workaround (codeship)
+if '/app' in os.environ['HOME']:
+    import django_heroku
+
+    django_heroku.settings(locals())
+
+# sslmode issue workaround (dev)
+# import django_heroku
+# django_heroku.settings(locals())
 # del DATABASES['default']['OPTIONS']['sslmode']
 
 REST_FRAMEWORK = {
